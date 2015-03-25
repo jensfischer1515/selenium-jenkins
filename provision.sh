e2d56ed1d5662163fb968ff1a0c570f55f73d190
Jens Fischer <j.fischer@epages.com>
2015-03-25 18:06:30 +0100

    jenkins and job config

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 
-apt-get install -y unzip git openjdk-7-jdk jenkins
+apt-get install -y unzip git openjdk-7-jdk jenkins gradle
 
 wget -qO- https://get.docker.com/ | sh
 
@@ -13,3 +13,6 @@ cp /vagrant/files/etc/default/docker /etc/default/docker
 service docker restart
 
 su - jenkins -c 'wget -q -O /var/lib/jenkins/plugins/docker-build-step.hpi http://updates.jenkins-ci.org/latest/docker-build-step.hpi'
+su - jenkins -c 'wget -q -O /var/lib/jenkins/plugins/gradle.hpi http://updates.jenkins-ci.org/latest/gradle.hpi'
+
+service jenkins restart
